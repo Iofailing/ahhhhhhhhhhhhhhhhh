1457f3d9d68e2d07bce6266708ab91425ea56876
018351 <018351@PC4225>
2022-09-29 11:36:13 +0100

    Add project files.

---
+++ NEA/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEA
+{
+    class Class1
+    {
+
+    }
+}
---
+++ NEA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using static System.Console;
+
+namespace NEA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("");
+        }
+        public static void Print(string input)
+        {
+
+            Thread.Sleep(15);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i]);
+                Thread.Sleep(15);
+            }
+            Console.WriteLine();
+        }
+    }
+}
