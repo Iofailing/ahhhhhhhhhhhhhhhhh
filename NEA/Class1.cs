15ddbf87738b2cbacb706b8f909074598d7f7170
Iofailing <114494169+Iofailing@users.noreply.github.com>
2022-10-07 14:32:25 +0100

    Update Class1.cs

--- NEA/Class1.cs
+++ NEA/Class1.cs
@@ -1,11 +1,56 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace NEA
 {
-    class Class1
+    class Basics_
     {
+        //Basics for people and enemies - living things that will most likely be in a fight
+        //so therefore need stats for attack, health, exp and level
+        public int hp;
+        public int attack;
+        public int exp;
+        public int level;
+    }
 
+    class Basics_weapons
+    {
+        public int attack;
+        public int defense;
+        public Basics_weapons(int attack1, int defense1)
+        {
+            attack = attack1;
+            defense = defense1;
+        }
+    }
+
+    class Basic_h_items
+    {
+        //basics for health items
+
+    }
+
+    class Player : Basics_
+    {
+        public Player(int hp1, int attack1, int exp1, int level1)
+        {
+            hp = hp1;
+            attack = attack1;
+            exp = exp1;
+            level = level1;
+        }
+    }
+
+    class Enemy : Basics_
+    {
+        public Enemy(int hp1, int attack1, int exp1, int level1)
+        {
+            //general class for all enemies and will be used to create multiple different types of enemy
+            hp = hp1;
+            attack = attack1;
+            exp = exp1;
+            level = level1;
+        }
     }
 }
