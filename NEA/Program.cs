79dc61406240583f69c69e8691a0045db10734ec
Iofailing <114494169+Iofailing@users.noreply.github.com>
2022-11-02 13:33:46 +0000

    Update Program.cs

--- NEA/Program.cs
+++ NEA/Program.cs
@@ -37,6 +37,30 @@ namespace NEA
 
             Print("You open your eyes and look around, seeing a huge clearing surrounded by giant pine trees that looked to be extremly old.\nYou look down and see your face reflected in the water 'Who am i?' you think to yourself");
             Print("What would you like to do?\nWould you like a tutorial?");
+            string userin3 = ReadLine();
+            Clear();
+
+            string Tutorial_Basics = "To move, type 'up', 'down', 'left', or 'right'\nIf you come across an enemy, you will have set choices to pick from\nTo pick up an item type 'pick up item' or 'add to inventory'\nTyping 'pick up item' will automatically add the item to your inventory";
+
+            if(userin3.ToLower == "yes")
+            {
+                Print(Tutorial_Basics);
+                Clear();
+            }
+            else
+            {
+                Print("Are you sure you wouldn't like a tutorial?");
+                string userin4 = ReadLine();
+                if(userin4.ToLower == "yes")
+                {
+                    Print("Alright; if youre sure then");
+                    Clear();
+                }
+                else
+                {
+                    Print(Tutorial_Basics);
+                }
+            }
             
             //.ToLower()
             //console.clear();
